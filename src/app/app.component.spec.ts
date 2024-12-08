b2564b245fb283c8a900218fd31e1c584048fa05
Viktor Ens <v.ens86@yahoo.de>
2024-12-08 14:01:36 +0100

    create basic pages, components and routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'join_ui' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('join_ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, join_ui');
-  });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { MainComponent } from '../pages/main/main.component';
 import { LoginComponent } from '../pages/login/login.component';
 import { SignupComponent } from '../pages/signup/signup.component';
+import { TaskComponent } from '../pages/task/task.component';
+import { LayoutComponent } from '../pages/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: MainComponent },
+  // { path: '', component: MainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: MainComponent },
+      { path: 'task', component: TaskComponent },
+    ],
+  },
 ];
