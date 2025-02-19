7cad5a813d3aecadf4443f2b13e3d4bd120539e4
DerEns86 <v.ens86@yahoo.de>
2025-02-19 13:58:29 +0100

    feat: implement contact management with display and delete functionality

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { ContactInterface } from '../../models/contact.interface';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  public contactService: ContactService = inject(ContactService);
 
+  contacts = this.contactService.contactSig;
+
+  deleteContact(contactId: number) {
+    this.contactService.deleteContact(contactId);
+  }
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { ContactInterface } from '../models/contact.interface';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
-  constructor() {}
+  http: HttpClient = inject(HttpClient);
+  readonly BASE_URL = environment.API_URL;
+
+  contactSig = signal<ContactInterface[]>([]);
+
+  constructor() {
+    this.fetchContacts();
+  }
+
+  fetchContacts() {
+    this.http.get<ContactInterface[]>(`${this.BASE_URL}api/contact`).subscribe({
+      next: (data) => {
+        this.contactSig.set(data);
+        console.log(this.contactSig());
+      },
+    });
+  }
+
+  getContacts() {
+    return this.contactSig;
+  }
+
+  deleteContact(contactId: number) {
+    this.http.delete(`${this.BASE_URL}api/contact/${contactId}`).subscribe({
+      next: () =>
+        this.contactSig.update((items) =>
+          items.filter((item) => item.id !== contactId)
+        ),
+    });
+  }
 }
