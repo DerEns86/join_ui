824854dc35622bef20df383a3493a5a7af47aed8
DerEns86 <v.ens86@yahoo.de>
2025-02-20 16:18:48 +0100

    style: update contact component style

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ContactService } from '../../services/contact.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DialogContactComponent } from '../../components/dialog-contact/dialog-contact.component';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
@@ -17,11 +18,26 @@ export class ContactComponent {
 
   contacts = this.contactService.contactSig;
 
+  coloredContacts = computed(() =>
+    this.contacts().map((contact) => ({
+      ...contact,
+      color: this.generateRandomColor(),
+    }))
+  );
+
   deleteContact(contactId: number) {
     this.contactService.deleteContact(contactId);
   }
 
   openModal() {
     const modalRef = this.modalService.open(DialogContactComponent);
+  }
+
+  getInitials(firstName: string, secondName: string) {
+    return firstName.charAt(0) + secondName.charAt(0);
+  }
+
+  generateRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
   }
 }
